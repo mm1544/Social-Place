c3f5c5f5c91dd64dbe0cfcbfb19995195801bd72
mminskis <martynas.minskis@jtrs.co.uk>
2023-04-19 19:26:00 +0100

    Add message entry form

--- base/views.py
+++ base/views.py
@@ -8,7 +8,7 @@ from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 # rooms = [
@@ -99,6 +99,16 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     # (!!) It is a special Dj way to get a Set of all the Messages of this Room (!!)
     room_messages = room.message_set.all().order_by('-created')
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
+        )
+        # I could not do 'redirect' and the page would be anyway refreshed, but I explicitly want this Page to fully reload (!!!)
+        return redirect('room', pk=room.id)
+
     context = {'room': room, 'room_messages': room_messages}
     return render(request, 'base/room.html', context)
 
