0f7bede4a451450bea90a9a1944d772b5ed99778
mminskis <martynas.minskis@jtrs.co.uk>
2023-04-22 17:53:49 +0100

    Add activity_components, topic_components and feed_components

--- base/views.py
+++ base/views.py
@@ -117,6 +117,16 @@ def room(request, pk):
     context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html', context)
 
+def userProfile(request, pk):
+    user = User.objects.get(id=pk)
+    # Getting all the children of 'room' by using '_set'
+    rooms = user.room_set.all()
+    # All the messages added in any room
+    room_messages = user.message_set.all()
+    topics = Topic.objects.all()
+    context = {'rooms': rooms,'user':user, 'room_messages': room_messages, 'topics': topics}
+    return render(request, 'base/profile.html', context)
+
 # Unauthorised User will be redirected to 'login' url
 @login_required(login_url='login')
 def createRoom(request):
